c5d55a2e0ca17103a055c068a64ac79f1b6b2726
TaeHwan <110373172+pth1004@users.noreply.github.com>
2022-10-10 17:40:02 +0900

    회원정보조회수정삭제 커밋

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,8 +3,54 @@ const { Owner } = require("../models");
 const bcrypt = require("bcrypt");
 
 exports.Ownerinfo = async (req, res) => {
-  try { /* passport면 req.body.id token이면 req.decoded.id */
-    const myprofile = await Owner.findOne({ where : { id : req.params.email}})
+  try {
+    /* passport면 req.body.id token이면 req.decoded.id */
+    const myprofile = await Owner.findOne({ where: { id: req.params.email } });
     console.log(myprofile);
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      message: "점주 정보가 없습니다.",
+    });
   }
-}
+};
+exports.editOwner = async (req, res) => {
+  try {
+    const { provider, password, name, ownerPhone } = req.body;
+    const renewOwner = await Owner.update(
+      {
+        provider,
+        password,
+        name,
+        ownerPhone,
+      },
+      {
+        where: { id: req.params.email },
+      }
+    );
+    console.log(renewOwner);
+    if (renewOwner) {
+      return res.status(200).json({
+        message: "정보 수정 성공",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      message: "Not Found",
+    });
+  }
+};
+exports.withdrawOwner = async (req, res) => {
+  try {
+    await Owner.destroy({ where: { id: req.body.email } });
+    res.status(200).json({
+      message: "점주 삭제 성공",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      message: "Not Found",
+    });
+  }
+};
--- routes/profile.js
+++ routes/profile.js
@@ -2,14 +2,13 @@ const express = require("express");
 const { isNotLoggedIn } = require("../middlewares/middleware");
 const {
   Ownerinfo,
-  // signin,
-  // checkEmail,
-  // checkUserId,
-} = require("../controllers/auth");
+  editOwner,
+  withdrawOwner,
+} = require("../controllers/profile");
 
 //점주 정보 조회
 router.get("/:email", isLoggedIn, Ownerinfo);
 //점주 정보 수정
-router.put("/edit-profile", isNotLoggedIn, signin);
+router.put("/edit-profile/:email", isLoggedIn, editOwner);
 //점주 정보 삭제
-router.delete("/remove/:email", isNotLoggedIn);
+router.delete("/remove-profile/:email", withdrawOwner);
