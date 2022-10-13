168f90faae26250ab9b23b283bc4d592ca13911d
박승규 <redpanda6801@gmail.com>
2022-10-13 22:35:29 +0900

    예외처리 lib 추가 및 수정 (removecafe 이전까지)

--- controllers/cafe.js
+++ controllers/cafe.js
@@ -1,13 +1,33 @@
 const Cafe = require("../models/cafe");
 const Owner = require("../models/owner");
+const { getExpireDate } = require("../libs/util");
+const resCode = require("../libs/error");
 
-const { getExpireDate } = require("../methods/code");
+exports.cafeinfo = async (req, res) => {
+  try {
+    const mycafes = await Cafe.findAll({ where: { id: req.decoded.id } });
+    // 카페 확인
+    const response = {};
+    if (mycafes === []) {
+      response = resCode.NO_SEARCH_DATA;
+    } else {
+      response = resCode.REQEST_SUCCESS;
+    }
+    response.data = mycafes;
+    return res.status(response.code).json(response);
+  } catch (error) {
+    console.error("ERROR :", error);
+    error.statusCode = 500;
+    next(error);
+  }
+};
 
 exports.addCafe = async (req, res) => {
   try {
     // 사업자 번호, 카페 이름, 카페 위치, 구독 월 수
     const { cafeName, location, businessNum, subscribeDate } = req.body;
-    console.log(businessNum, cafeName, location, subscribeDate);
+
+    // 카페 중복 예외처리 필요!
     const newCafe = await Cafe.create({
       cafeName: cafeName,
       location: location,
@@ -15,89 +35,41 @@ exports.addCafe = async (req, res) => {
       expireDate: getExpireDate(subscribeDate),
       OwnerId: req.decoded.id,
     });
-    console.log(newCafe);
-    if (newCafe) {
-      return res.status(200).json({
-        message: "Create Success",
-        cafename: cafeName,
-      });
+    const cafeData = newCafe.dataValues;
+    // 카페 생성 확인 예외처리
+    if (!cafeData || cafeData === {}) {
+      const error = resCode.BAD_REQEST_WRONG_DATA;
+      return res.status(error.code).json(error);
     } else {
-      console.log("Failed to Create");
-      return res.status(400).json({
-        message: "Failed to Create",
-      });
+      const response = resCode.REQEST_SUCCESS;
+      response.cafe = cafeData;
+      return res.status(response.code).json(response);
     }
   } catch (error) {
-    console.log(error);
-    return res.status(404).json({
-      message: "Not Found",
-    });
-  }
-};
-
-exports.checkCafeName = async (req, res) => {
-  try {
-    // 사업자 번호, 카페 이름, 카페 위치, 구독 월 수
-    const { cafeName } = req.params;
-    const cafe = await Cafe.findOne({ where: { cafeName } });
-    if (cafe.cafeName === cafeName) {
-      return res.status(400).json({
-        message: "Name Already Existed",
-      });
-    } else {
-      return res.status(200).json({
-        message: "Name Check Success",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(404).json({
-      message: "Not Found",
-    });
-  }
-};
-exports.cafeinfo = async (req, res) => {
-  try {
-    const { id } = req.decoded;
-    const owner = await Owner.findOne({ where: { id } });
-    const mycafe = await Cafe.findOne({ where: { id } });
-
-    console.log(mycafe);
-    if (mycafe) {
-      return res.status(200).json({
-        message: "Cafe Infomation Success",
-        data: {
-          name: mycafe.cafeName,
-          location: mycafe.location,
-          bsNum: mycafe.businessNum,
-          expire: mycafe.expireDate,
-          icon: mycafe.icon,
-          img: mycafe.img,
-          phone: owner.ownerPhone,
-        },
-      });
-    } else {
-      return res.status(403).json({
-        message: "Forbidden",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(404).json({
-      message: "Not Found",
-    });
+    console.error("ERROR :", error);
+    error.statusCode = 500;
+    next(error);
   }
 };
 
 exports.updatecafe = async (req, res) => {
   try {
+    // 수정할 값은 하나만 들어와도 수정되어야 한다.
     const { cafeName, location, businessNum } = req.body;
-    console.log(req.files);
-    const icon = req.files[1] && req.files[1].filename;
-    const img = req.files[0] && req.files[0].filename;
-    const cafe = await Cafe.findOne({ where: { id: req.decoded.id } });
-    if (cafe) {
-      const renewCafe = await Cafe.update(
+    const { cafeId } = req.params;
+    const formData = req.files;
+
+    const icon = formData[1] && formData[1].filename;
+    const img = formData[0] && formData[0].filename;
+
+    if (
+      !(await Cafe.findOne({ where: { id: cafeId, OwnerId: req.decoded.id } }))
+    ) {
+      const response = resCode.NO_SEARCH_DATA;
+      console.log(response.message);
+      return res.status(response.code).json(response);
+    } else {
+      await Cafe.update(
         {
           cafeName: cafeName ? cafeName : cafe.cafeName,
           location: location ? location : cafe.location,
@@ -109,28 +81,17 @@ exports.updatecafe = async (req, res) => {
           where: { id: req.decoded.id },
         }
       );
-      if (renewCafe) {
-        return res.status(200).json({
-          message: "Update Success",
-        });
-      } else {
-        return res.status(400).json({
-          message: "Update Failed",
-        });
-      }
-    } else {
-      return res.status(403).json({
-        message: "Forbidden",
-      });
+      const response = resCode.REQEST_SUCCESS;
+      return res.status(response.code).json(response);
     }
   } catch (error) {
-    console.log(error);
-    return res.status(404).json({
-      message: "Not Found",
-    });
+    console.error("ERROR :", error);
+    error.statusCode = 500;
+    next(error);
   }
 };
 
+// 여기부터 내일 해야함
 exports.removecafe = async (req, res) => {
   try {
     const { cafeName } = req.params;
@@ -146,6 +107,29 @@ exports.removecafe = async (req, res) => {
       console.log("cafeName error");
       res.status(400).json({
         message: "cafeName Error",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      message: "Not Found",
+    });
+  }
+};
+
+// 카페이름 중복 확인 API, 사용 여부 확인 필요
+exports.checkCafeName = async (req, res) => {
+  try {
+    // 사업자 번호, 카페 이름, 카페 위치, 구독 월 수
+    const { cafeName } = req.params;
+    const cafe = await Cafe.findOne({ where: { cafeName } });
+    if (cafe.cafeName === cafeName) {
+      return res.status(400).json({
+        message: "Name Already Existed",
+      });
+    } else {
+      return res.status(200).json({
+        message: "Name Check Success",
       });
     }
   } catch (error) {
