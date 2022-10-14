91a6a3d6aadf4c1a89bddfd96b3cad7017def261
TaeHwan <110373172+pth1004@users.noreply.github.com>
2022-10-14 11:01:11 +0900

    customer, find, mail 오류처리 커밋

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,26 +1,23 @@
 const { Stamp } = require("../models");
 
 // 카페마다의 고객정보를 조회할 수 있다.
-exports.infoCustomer = async (req, res) => {
+exports.infoCustomer = async (req, res, next) => {
   try {
     const { cafeId } = req.params;
 
     const custStamp = await Stamp.findAll({ where: { CafeId: cafeId } });
     if (custStamp.length) {
-      return res.status(200).json({
-        message: "Customer Search Success",
-        customers: custStamp,
-      });
+      const response = resCode.REQEST_SUCCESS;
+      response.customers = custStamp;
+      return res.status(response.code).json(response);
     } else {
       console.log("No Customer in your Cafe");
-      return res.status(204).json({
-        meesage: "No Customer in you Cafe",
-      });
+      const error = resCode.BAD_REQUEST_NO_USER;
+      return res.status(error.code).json({ error });
     }
   } catch (error) {
-    console.log(error);
-    return res.status(404).json({
-      message: "Not Found",
-    });
+    console.error("ERROR :", error);
+    error.statusCode = 500;
+    next(error);
   }
 };
